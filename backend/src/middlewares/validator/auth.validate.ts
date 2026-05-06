7bc0d9c00fa7ef96267be43d56e3c1ed0b253751
Govind <govindgothi5815@gmail.com>
2026-05-06 09:05:26 +0530

    add zod validatin for user register api

---
+++ backend/src/middlewares/validator/auth.validate.ts
@@ -0,0 +1,17 @@
+
+import { Request, Response, NextFunction } from "express";
+
+import { formatZodError } from "../../validations/common/zodError.validations.js";
+import { ZodType   } from "zod";
+
+export const validate =
+  <T>(schema: ZodType<T>) =>
+  (req: Request, res: Response, next: NextFunction) => {
+    const result = schema.safeParse(req.body);
+    if (!result.success) {
+      return res.status(400).json(formatZodError(result.error));
+    }
+    // attach validated data (important)
+    req.body = result.data;
+    next();
+  };
