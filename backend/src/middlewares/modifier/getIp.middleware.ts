f7a2ff66021c869280a59d268d8a00b07a42768f
Govind <govindgothi5815@gmail.com>
2026-05-06 09:07:04 +0530

    add middleware for get ip and modify req data

---
+++ backend/src/middlewares/modifier/getIp.middleware.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response } from "express";
+
+const getClientIp = (req:Request, _res: Response, next: NextFunction) => {
+  let ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress || '';
+
+  // If array → take first value
+  if (Array.isArray(ip)) {
+    ip = ip[0];
+  }
+
+  // If comma-separated → take first IP
+  if (typeof ip === 'string' && ip.includes(',')) {
+    ip = ip.split(',')[0].trim();
+  }
+
+  req.body.clientIp = ip;
+  next();
+};
+
+export default getClientIp;
