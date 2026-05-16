a8681be5965b09ce5b932c5c71bf0ad91d0c24f5
Govind <govindgothi5815@gmail.com>
2026-05-16 08:07:42 +0530

    added bullmq instance for worker

---
+++ backend/src/db/bullmq.ts
@@ -0,0 +1,8 @@
+import { Redis } from 'ioredis';
+
+export const connection = new Redis({
+  host: process.env.REDIS_HOST ?? 'localhost',
+  port: Number(process.env.REDIS_PORT) ?? 6379,
+  // password: process.env.REDIS_PASSWORD,
+  maxRetriesPerRequest: null, // Required by BullMQ
+});
