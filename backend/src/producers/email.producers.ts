e5a8a557565039554237a74eb67e77872128ce14
Govind <govindgothi5815@gmail.com>
2026-05-16 08:16:04 +0530

    add email producers

---
+++ backend/src/producers/email.producers.ts
@@ -0,0 +1,25 @@
+import { MailBodyProducer } from "../interface/auth.interface.js";
+import { emailQueue } from "../queues/queue.js";
+
+export const addEmailJob = async({to,subject,body}:MailBodyProducer)=> {
+  await emailQueue.add(
+    "send-register-otp-email",
+    {
+      to,
+      subject,
+      body,
+    },
+    {
+      attempts: 3,
+      backoff: {
+        type: "exponential",
+        delay: 3000,
+      },
+      removeOnComplete: true,
+      removeOnFail: false,
+    }
+  );
+
+  console.log("Job added");
+}
+
