aa878b499ff9b18bf80bb5dafe4ae8e6d544462e
Govind <govindgothi5815@gmail.com>
2026-05-16 08:13:20 +0530

    add interfaces for auth controllers

--- backend/src/interface/auth.interface.ts
+++ backend/src/interface/auth.interface.ts
@@ -1,5 +1,51 @@
+export interface IpParams {
+  ip: string;
+}
+
+export interface Id {
+  id: number;
+}
+
 export type GetUserParams = {
   email?: string;
   id?: number;
   fields: string[];
 };
+
+export type EmailParams = {
+  email: string;
+};
+
+export type AddOtpLogsModel = {
+  ip: string;
+  userId: number;
+  otpHash: string;
+};
+
+export type UpdateOtpLogModel = {
+  email: string;
+  verified?: boolean;
+};
+
+export type RegisterUserModel = {
+  username: string;
+  email: string;
+  password: string;
+  profileUrl?: string;
+};
+
+export type MailBodyProducer = {
+  to: string;
+  body: string;
+  subject: string;
+};
+
+export type updateEmailBlock = {
+  email:string,
+  redisBlockedEmailKey:string
+}
+
+export type updateIpBlockType = {
+  ip: string,
+  redisBlockedIpKey: string
+}
