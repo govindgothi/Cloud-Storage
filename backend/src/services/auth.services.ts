eb86e9927106f392a986675e8fa6f329cbb83a10
Govind <govindgothi5815@gmail.com>
2026-05-16 09:14:56 +0530

    update otp attempts

--- backend/src/services/auth.services.ts
+++ backend/src/services/auth.services.ts
@@ -12,36 +12,58 @@ import { UserRegisterInput } from "../validations/auth.validations.js";
 import { generateSixDigitCode } from "../utils/common.utils.js";
 import { getRedisClient } from "../db/redis.db.js";
 import { successResponse } from "../utils/responseHandler.js";
-import { addOtpLogProducer, updateEmailBlockStatusProducer, updateIpBlockStatusProducer } from "../producers/otpLogs.producers.js";
+import {
+  addOtpLogProducer,
+  updateEmailBlockStatusProducer,
+  updateIpBlockStatusProducer,
+} from "../producers/otpLogs.producers.js";
 import { hashValue, verifyHash } from "../utils/bcrypt.utils.js";
 
 export const userRegisterService = async (data: UserRegisterInput) => {
-  const redis = getRedisClient()
+  const redis = getRedisClient();
+
   const { username, email, password, otp } = data;
   const profileUrl = getProfileUrl(username);
   const fields = ["id", "email"];
 
   const isUser = await getUsersDetailByEmail({ fields, email });
   console.log("isUser", isUser);
+
   if (isUser && isUser.length) {
     throw new ApiError("This Email is already exists", 403, false);
   }
-   const otpDataKey = `otp:data:${email}`;
-   const otpData = await redis.get(otpDataKey);
-   if(!otpData || otpData == null){
-    throw new ApiError("Otp is expired",401,false)
-   }
-  const parsedOtp = JSON.parse(otpData)
-  const isOtpMatched = await verifyHash(otp,parsedOtp.otp)
-  if(isOtpMatched == false || !isOtpMatched){
-    
-    throw new ApiError("otp is not matched",401,false)
+
+  const otpDataKey = `otp:data:${email}`;
+  const otpData = await redis.get(otpDataKey);
+
+  let otpParseData = otpData ? JSON.parse(otpData) : {};
+  if(!otpParseData || otpParseData == null){
+   throw new ApiError("Otp is expired",401,false)
   }
-  const hashPassword =  await hashValue(password)
+  if(otpParseData.attempts > 3){
+   throw new ApiError("You hit max attempt",401,false)
+  }
+  otpParseData.attempts = (otpParseData.attempts || 0) + 1;
+
+  await redis.set(otpDataKey, JSON.stringify(otpParseData), {
+    EX: 120, // 2 minutes
+  });
+
+  if (!otpData || otpData == null) {
+    throw new ApiError("Otp is expired", 401, false);
+  }
+
+  const parsedOtp = JSON.parse(otpData);
+  const isOtpMatched = await verifyHash(otp, parsedOtp.otp);
+  if (isOtpMatched == false || !isOtpMatched) {
+    throw new ApiError("otp is not matched", 401, false);
+  }
+
+  const hashPassword = await hashValue(password);
   const userStore = await registerQuery({
     username,
     email,
-    password:hashPassword,
+    password: hashPassword,
     profileUrl,
   });
   if (userStore.affectedRow == 1 && userStore.insertedId) {
@@ -52,7 +74,6 @@ export const userRegisterService = async (data: UserRegisterInput) => {
     );
   }
 };
-
 
 export const sendOtpService = async ({
   email,
@@ -85,7 +106,11 @@ export const sendOtpService = async ({
 
   if (isIpBlocked) {
     const ttl = await redis.ttl(redisBlockedIpKey);
-    throw new ApiError(`Too many requests. Try again in ${ttl} seconds`, 429, false,);
+    throw new ApiError(
+      `Too many requests. Try again in ${ttl} seconds`,
+      429,
+      false,
+    );
   }
 
   // check, Is given email block or not
@@ -118,13 +143,12 @@ export const sendOtpService = async ({
     });
     const ttl = await redis.ttl(redisBlockedEmailKey);
     // --add email and ip in queue for store in backend sql db
-    updateEmailBlockStatusProducer({email,redisBlockedEmailKey})
+    updateEmailBlockStatusProducer({ email, redisBlockedEmailKey });
     throw new ApiError(
       `Too many requests on Email. Try again in ${ttl} seconds`,
       429,
       false,
     );
-    
   }
 
   // set block limit 30 minute whose send request of 5 otp with in 15 min on this ip
@@ -134,41 +158,39 @@ export const sendOtpService = async ({
     });
     const ttl = await redis.ttl(redisBlockedIpKey);
     // --add ip and email in queue for store in backend sql db
-    updateIpBlockStatusProducer({ip:clientIp,redisBlockedIpKey})
+    updateIpBlockStatusProducer({ ip: clientIp, redisBlockedIpKey });
     throw new ApiError(
       `Too many requests on Ip. Try again in ${ttl} seconds`,
       429,
       false,
     );
-   
   }
 
   const [blockedIps, blockedEmails] = await Promise.all([
     getBlockedIps({ ip: clientIp }),
     getBlockedEmails({ email }),
   ]);
-  console.log(blockedIps,blockedEmails)
-  
-  let ipId: number = blockedIps[0].id
-  let emailId: number = blockedEmails[0].id
+  console.log(blockedIps, blockedEmails);
 
+  let ipId: number = blockedIps[0].id;
+  let emailId: number = blockedEmails[0].id;
 
-  if (!(blockedIps.length>0)) {
+  if (!(blockedIps.length > 0)) {
     const ipdata = await insertBlockedIp({ ip: clientIp });
     ipId = ipdata.insertedId;
   }
-  if (!(blockedEmails.length>0)) {
+  if (!(blockedEmails.length > 0)) {
     const emaildata = await insertBlockedEmail({ email });
     emailId = emaildata.insertedId;
   }
 
   const otpCode = generateSixDigitCode();
-  const otpHash = await hashValue(otpCode)
+  const otpHash = await hashValue(otpCode);
   const payload = {
     email,
     clientIp,
-    otpCode:otpHash,
-    attempt: 0,
+    otpCode: otpHash,
+    attempts: 0,
   };
 
   // Store JSON string with expiry
