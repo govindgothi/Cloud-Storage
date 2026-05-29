64308b89b37053d6e21a4306ca53bebffdd764a8
Govind <govindgothi5815@gmail.com>
2026-05-29 09:39:20 +0530

    add common interface file

---
+++ backend/src/interface/common.interface.ts
@@ -0,0 +1,15 @@
+export type EmailParams = {
+  email: string;
+};
+
+export type idParams = {
+  id:string
+}
+
+export type userIdParams ={
+  userId:string
+}
+
+export type clientIpParams = {
+  clientIp:string
+}
