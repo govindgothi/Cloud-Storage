c566426bc8ee67688afa63a7308e2123b123b47f
Govind <govindgothi5815@gmail.com>
2026-05-29 09:40:03 +0530

    add register route and login

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
-import { sendOtp, userRegister } from "../controllers/auth.controllers.js";
+import { sendOtp, userRegister,userLogin } from "../controllers/auth.controllers.js";
 import { userRegisterSchema } from "../validations/auth.validations.js";
 import { validate } from "../middlewares/validator/auth.validate.js";
 import getClientIp from "../middlewares/modifier/getIp.middleware.js";
 
 const router = Router() 
 
-router.post("/",validate(userRegisterSchema), userRegister)
+router.post("/register",validate(userRegisterSchema), userRegister)
 router.post("/send-otp",getClientIp,sendOtp)
+router.post("/login",userLogin)
 
 export default router;
