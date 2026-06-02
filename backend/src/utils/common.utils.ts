336e639306d1929c7cba55be70917330bfdabf40
Govind <govindgothi5815@gmail.com>
2026-06-02 09:41:58 +0530

    add random uuid function

--- backend/src/utils/common.utils.ts
+++ backend/src/utils/common.utils.ts
@@ -3,3 +3,7 @@ import crypto from 'crypto';
 export const generateSixDigitCode = (): string => {
   return crypto.randomInt(100000, 1000000).toString();
 };
+
+export const generateRandomUUId = ():string =>{
+  return crypto.randomUUID()
+}
